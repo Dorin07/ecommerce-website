3cace32930fab5c2dbf2057a8c28dc2dc983aa93
Dorin07 <47871786+Dorin07@users.noreply.github.com>
2021-11-09 12:02:01 +0200

    added the data and configure the css style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code> src/App.js </code> and save to reload then check github.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      
+      <div class="row">
+        <div class="col-6">Column number 1</div>
+        <div class="col-6">
+          <span>
+            <i className="fas fa-home"></i>
+          </span>
+        </div>
+      </div>
+      
     </div>
   );
 }
