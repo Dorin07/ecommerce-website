0c55229796da19eb673e5da96645540d90b93115
Dorin07 <47871786+Dorin07@users.noreply.github.com>
2021-11-10 10:51:06 +0200

    Create the navbar.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
-
-function App() {
-  return (
-    <div class="container">
-      
-      <div class="row">
-        <div class="col-6">Column number 1</div>
-        <div class="col-6">
-          <span>
-            <i className="fas fa-home"></i>
-          </span>
-        </div>
-      </div>
-      
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <Navbar></Navbar>
+        <Routes>
+          <Route exact path="/" element={<ProductList/>}></Route>
+          <Route exact path="/details" element={<Details/>}></Route>
+          <Route exact path="/cart" element={<Cart/>}></Route>
+          <Route path="*" element={<Default/>}></Route>
+        </Routes>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
